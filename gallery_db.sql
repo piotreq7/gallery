3bfada62eed1e349a7f3524ee6366d4060634370
piotreq7 <45266559+piotreq7@users.noreply.github.com>
2019-03-11 09:10:15 +0100

    zz

--- gallery_db.sql
+++ gallery_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 03 Mar 2019, 21:33
+-- Czas generowania: 11 Mar 2019, 09:09
 -- Wersja serwera: 10.1.37-MariaDB
 -- Wersja PHP: 7.3.1
 
@@ -21,6 +21,35 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `gallery_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `photos`
+--
+
+CREATE TABLE `photos` (
+  `photo_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `photos`
+--
+
+INSERT INTO `photos` (`photo_id`, `title`, `description`, `filename`, `type`, `size`) VALUES
+(1, 'tytu?', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'filename', 'type', 12),
+(2, 'tytul', 'descriptio', 'filename', 'type', 12),
+(3, 'Example_photo', '', '', '', 20),
+(4, 'Example_photo', '', '', '', 20),
+(5, 'Example_photo', '', '', '', 20),
+(6, '23', '', 'images_2.jpg', 'image/jpeg', 18578),
+(7, '1', '', 'image-1 copy.jpg', 'image/jpeg', 328747),
+(8, '', '', 'image-1.jpg', 'image/jpeg', 328747);
 
 -- --------------------------------------------------------
 
@@ -43,11 +72,33 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`) VALUES
 (1, 'kielich', '123', 'Karolina', 'Kielich'),
 (2, 'marczyk', '123', 'Kazimierz', 'Marczyk'),
-(3, 'test', 'test', 'test', 'test');
+(8, '', '', '', 'Babaa'),
+(10, 'Example_username', 'Example_password', 'Joe', 'Babaa'),
+(11, 'Example_username', 'Example_password', 'Joe', 'Babaa'),
+(12, 'Example_username', 'Example_password', 'Joe', 'Babaa'),
+(13, 'Example_username', 'Example_password', 'Joe', 'Babaa'),
+(14, 'Example_username', 'Example_password', 'Joe', 'Babaa'),
+(15, 'Example_username', 'Example_password', 'Joe', ''),
+(16, 'Example_username', 'Example_password', 'Joe', ''),
+(17, 'Example_username', 'Example_password', 'Joe', ''),
+(18, 'Example_username', 'Example_password', 'Joe', ''),
+(19, '', '', '', 'aaaaaa'),
+(20, '', '', '', 'Babaasssssss'),
+(21, 'Example_username', 'Example_password', 'Joe', ''),
+(22, 'Example_username', 'Example_password', 'Joe', ''),
+(23, 'Example_username', 'Example_password', 'Joe', 'Babaa'),
+(25, 'Example_username', 'Example_password', 'Joe', 'Babaa'),
+(26, 'Example_username', 'Example_password', 'Joe', 'Babaa');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`photo_id`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -60,10 +111,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `photo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
